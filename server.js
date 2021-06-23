f800a01500bc402872b00a950051bfb3707d5841
edsont8 <edsont8@hotmail.com>
2021-06-23 09:31:55 -0300

    final

--- server.js
+++ server.js
@@ -4,14 +4,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 4000;
 
-server.use(function(req, res, next) {
-    setTimeout(next, 10000);
-});
-
 server.use(middlewares);
 server.use(router);
-server.listen(3000, function () {
-    console.log('JSON Server is running on heroku host.');
-    done();
-});
+
 server.listen(port);
